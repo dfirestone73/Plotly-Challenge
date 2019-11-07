4bb6d5b0040796b5c0877dd415f1b0ddf789e5d9
dfirestone73 <52640393+dfirestone73@users.noreply.github.com>
2019-11-06 20:22:10 -0600

    pushing

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,11 +1,11 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-  var url = "/metadata/<sample>"
+  var urlMeta = `/metadata/${sample}`;
 
   // Use `d3.json` to fetch the metadata for a sample
   // Use d3 to select the panel with id of `#sample-metadata`
-  d3.json(url).then(function (Bdata) {
+  d3.json(urlMeta).then(function (Bdata) {
 
     var panel = d3.select("#sample-metadata")
     // Use `.html("") to clear any existing metadata
@@ -25,18 +25,56 @@ function buildMetadata(sample) {
 };
 
 function buildCharts(sample) {
-  var data = '../../../../DataSets/belly_button_data.csv'
+  var urlChart = `/sample/${sample}`;
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(data).then(function (sample) {
-    var Sdata = [sample];
+  d3.json(urlChart).then(function (response) {
+
+    // @TODO: Build a Bubble Chart using the sample data
+    var desired_maximum_marker_size = 40;
+    var size = response.sample_values;
+    var trace1 = {
+      x: response.otu_ids,
+      y: response.sample_values,
+      text: ['A</br>size: 40</br>sixeref: 1.25', 'B</br>size: 60</br>sixeref: 1.25', 'C</br>size: 80</br>sixeref: 1.25', 'D</br>size: 100</br>sixeref: 1.25'],
+      mode: 'markers',
+      marker: {
+        size: size,
+        //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
+        sizeref: 2.0 * Math.max(...size) / (desired_maximum_marker_size ** 2),
+        sizemode: 'area'
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: 'Belly Button Samples',
+      showlegend: false,
+      height: 600,
+      width: 1000,
+      xaxis: { title: 'Sample ID' },
+      yaxis: { title: 'Sample Volume' }
+    };
+
+    Plotly.newPlot('bubble', data, layout);
+
+    // @TODO: Build a Pie Chart
+    var Pvalues = response.sample_values.slice(0, 9);
+    var Plabels = response.otu_ids.slice(0, 9);
+
+    var pchart = [{
+      values: Pvalues,
+      labels: Plabels,
+      type: 'pie'
+    }];
+
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+
+
   });
-  // @TODO: Build a Bubble Chart using the sample data
 
-  // @TODO: Build a Pie Chart
-
-  // HINT: You will need to use slice() to grab the top 10 sample_values,
-  // otu_ids, and labels (10 each).
-}
+};
 
 function init() {
   // Grab a reference to the dropdown select element
